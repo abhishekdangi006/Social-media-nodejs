1848b5950c0ae821c087029a91ca8d7902391564
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-01-22 16:09:58 +0530

    The router has been added to the express application.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// using the express router
+app.use('/', require('./routes'));
 app.listen(port, function(err){
     if(err){
         console.log(`Error is showing while running express server: ${err}`);
