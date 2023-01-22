969ab04c5c0a0679d7a739cfa7d42a2d96ca21c7
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-01-22 16:52:07 +0530

    Home controller has been added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const controller = require("../controller/home_controller");
+
+router.get('/', controller.home);
+
 console.log("Router has been started");
 module.exports = router;
