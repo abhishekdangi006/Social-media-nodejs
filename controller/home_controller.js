2dc40c79e00e563c4bdf0b2ac7e1f79c187809cb
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-02-05 20:57:17 +0530

    using async await on home controller

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,32 +1,24 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-
-    // Post.find({}, function(err, posts){
-    //     if(err){
-    //         console.log("error in home controller while showing posts");
-    //     }
-    //     return res.render('home',{
-    //         title: 'Home page',
-    //         posts: posts
-    //     });
-    // });
-
-    // populate the user of each post 
-    Post.find({}).populate('user').populate({
-        path: 'comments',
-        populate:{
-            path: 'user'
-        }
-    }).exec(function(err,posts){
-        User.find({},function(err,users){
-        return res.render('home',{
-        title: 'Home page',
-        posts: posts,
-        all_users:users
-    });
-});     
-    });
-    
+module.exports.home = async function(req,res){
+    // populate the user of each post
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate:{
+                path: 'user'
+            }
+        });
+        let users = await User.find({});
+            return res.render('home',{
+            title: 'Home page',
+            posts: posts,
+            all_users:users
+        });
+    } catch(err){
+        console.log("Error in home controller", err);
+    }
 }
